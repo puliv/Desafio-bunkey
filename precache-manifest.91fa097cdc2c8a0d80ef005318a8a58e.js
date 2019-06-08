130aef8943f3f5b3b8bd923747444a8e19f62738
puliv <pverapavez@gmail.com>
2019-06-07 20:17:21 -0400

    Updates

---
+++ precache-manifest.91fa097cdc2c8a0d80ef005318a8a58e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65dba32ce2476fcd83f2fd5715719b34",
+    "url": "/Desafio-bunkey/index.html"
+  },
+  {
+    "revision": "e1665d072483c2f4e51c",
+    "url": "/Desafio-bunkey/static/css/main.09a880d7.chunk.css"
+  },
+  {
+    "revision": "a449ff949bb7c1b700e5",
+    "url": "/Desafio-bunkey/static/js/2.933d840c.chunk.js"
+  },
+  {
+    "revision": "e1665d072483c2f4e51c",
+    "url": "/Desafio-bunkey/static/js/main.c138b363.chunk.js"
+  },
+  {
+    "revision": "1c3f34cc6693e1c6d7a6",
+    "url": "/Desafio-bunkey/static/js/runtime~main.948e7b43.js"
+  }
+]);
